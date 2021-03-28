d422d4d0f832a55f7c3b464de349ed106f19a7dc
Trufanov Roman <pingvin1308@gmail.com>
2021-03-28 16:10:44 +0300

    init paypal tests project

---
+++ BestCompany.Shop/TestPayPalIntegration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TestPayPalIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            var response = httpClient.GetAsync("https://paypal.com");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
