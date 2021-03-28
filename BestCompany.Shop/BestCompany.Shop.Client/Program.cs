07d62212b3b9671f859f08628903e3dcf3515032
Trufanov Roman <pingvin1308@gmail.com>
2021-03-28 16:28:33 +0300

    some changes

--- BestCompany.Shop/BestCompany.Shop.Client/Program.cs
+++ BestCompany.Shop/BestCompany.Shop.Client/Program.cs
@@ -7,6 +7,7 @@ namespace BestCompany.Shop.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
